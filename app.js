84082a2cb992607e8bbae6efd24986f4188fa81b
mbenson025 <mbenson025@gmail.com>
2022-10-17 22:28:59 -0500

    setup express server and middleware

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+//bodyParser for parsing middleware
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+require('dotenv').config();
+
+//initialize express
+const app = express();
+
+//set up a port
+const port = process.env.PORT || 5000;
+
+//parsing middleware
+
+//bodyParser.urlencoded returns middleware that only parses urlencoded bodies - it only looks at requests where 'Content-Type' header matches the type option.
+//the new body object will contain key-value pairs where the value can be a string or array (when extended is false), or any type(when extended is true)
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse application/json
+app.use(bodyParser.json());
+
+//listen to the port
+app.listen(port, () => console.log(`Listening on port ${port}`));
